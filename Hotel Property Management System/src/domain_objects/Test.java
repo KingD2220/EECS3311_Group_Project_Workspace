d9a85815ca0a093eb0e1392020e10183e6cc6c8a
BlockheadJ <93233333+BlockheadJ@users.noreply.github.com>
2023-02-01 20:31:25 -0500

    Reservation Class

--- Hotel Property Management System/src/domain_objects/Test.java
+++ Hotel Property Management System/src/domain_objects/Test.java
@@ -3,8 +3,12 @@ package domain_objects;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Reservation newRes = new Reservation("Bond", "James", "69 Fake St., Toronto, Canada", "4165555555", "4512565678432394");
+		
+		System.out.println(newRes.profile.getFirst_name());
+		System.out.println(newRes.profile.getAddress());
+		System.out.println(newRes.profile.getCredit_card());
 
 	}
-
+	
 }
