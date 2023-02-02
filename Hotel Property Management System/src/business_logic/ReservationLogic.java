7fd9b064216b7660eaff90da52ecdff320e875ed
Miguel-Graham <grahammiguel17@yahoo.com>
2023-02-02 16:43:05 -0500

    Starting Logic

---
+++ Hotel Property Management System/src/business_logic/ReservationLogic.java
@@ -0,0 +1,26 @@
+package business_logic;
+
+import java.util.List;
+
+import domain_objects.Reservation;
+import persistence.DatabaseStubs;
+
+public class ReservationLogic {
+	public Reservation addReservation() {
+		return null;
+		
+	}
+
+public void removeReservation(String resNum) {
+	
+}
+public Reservation updatReservation(Reservation reservation){
+	return reservation;
+	
+}
+public List<Reservation> getAllReservations() {
+	return DatabaseStubs.getReservations();
+	
+}
+
+}
--- Hotel Property Management System/src/persistence/DatabaseStubs.java
+++ Hotel Property Management System/src/persistence/DatabaseStubs.java
@@ -3,16 +3,19 @@ package persistence;
 import java.util.ArrayList;
 import java.util.List;
 
+import domain_objects.Profile;
+import domain_objects.Reservation;
+
 public class DatabaseStubs {
-	private static List<String> customers = new ArrayList<String>();
-	private static List<String> reservations = new ArrayList<String>();
-	private static List<String> employee = new ArrayList<String>();
-	private static List<String> rooms = new ArrayList<String>();
+	private static List<Profile> customers = new ArrayList<>();
+	private static List<Reservation> reservations = new ArrayList<>();
+	private static List<String> employee = new ArrayList<>();
+	private static List<String> rooms = new ArrayList<>();
 	
-	public static List<String> getCustomers() {
+	public static List<Profile> getCustomers() {
 		return customers;
 	}
-	public static List<String> getReservations() {
+	public static List<Reservation> getReservations() {
 		return reservations;
 	}
 	public static List<String> getEmployee() {
