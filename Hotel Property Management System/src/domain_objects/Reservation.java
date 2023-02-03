5977ebb0eb33fd08764506c74dd57b5f8b18c1e0
KingD2220 <kingd2220@gmail.com>
2023-02-03 13:07:00 -0500

    GUI creates reservation (minus room and services)

--- Hotel Property Management System/src/domain_objects/Reservation.java
+++ Hotel Property Management System/src/domain_objects/Reservation.java
@@ -1,9 +1,13 @@
 package domain_objects;
 
+import java.util.ArrayList;
+
 public class Reservation {
+	private static ArrayList<Reservation> reservationList = new ArrayList<>(); //List of reservations
+	
 	private String arrival_date = "";
 	private String departure_date = "";
-	Profile profile = new Profile();
+	public Profile profile = new Profile();
 	
 	public Reservation(String last_name, String first_name, String address, String phone_num, String credit_card) {
 		profile.setLast_name(last_name);
@@ -29,6 +33,7 @@ public class Reservation {
 		this.departure_date = departure_date;
 	}
 	
-	 
-	
+	public static ArrayList<Reservation> getList() {
+		return reservationList;
+	}
 }
