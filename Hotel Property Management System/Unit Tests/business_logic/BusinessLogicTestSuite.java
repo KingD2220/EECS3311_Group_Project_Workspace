b5823c87f350565481e39b8d912bf8100f3cd4be
humud99 <humud99@my.yorku.ca>
2023-03-10 03:35:44 -0500

    Refactoring

---
+++ Hotel Property Management System/Unit Tests/business_logic/BusinessLogicTestSuite.java
@@ -0,0 +1,11 @@
+package business_logic;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ReservationLogicTest.class })
+public class BusinessLogicTestSuite {
+
+}
