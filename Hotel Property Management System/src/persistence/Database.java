ae2c59481c572d9178f65e104a9c155fdaba2211
James A <jamesahn@my.yorku.ca>
2023-04-03 13:03:31 -0400

    Update Database.java

--- Hotel Property Management System/src/persistence/Database.java
+++ Hotel Property Management System/src/persistence/Database.java
@@ -17,5 +17,5 @@ public interface Database {
 	public void updateRoomStatus(String roomNum, String roomStatus);
 	public Employee getEmployee(String employeeNum);
 	public ArrayList<Reservation> getResByDate(String date, String caller);
-	public boolean checkInReservation(int resNum, String roomNum);
+	public boolean updateResStatus(int resNum, String roomNum);
 }
